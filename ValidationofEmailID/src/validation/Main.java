855751662c49c62f49c5dd3bacc0250049446a39
Akho10 <APhungula@sars.gov.za>
2026-05-04 16:47:54 +0200

    Added search for employee name operation

--- ValidationofEmailID/src/validation/Main.java
+++ ValidationofEmailID/src/validation/Main.java
@@ -1,10 +1,29 @@
 package validation;
-
+import java.util.*;
+ 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] employees = {"Akhona", "Juliet", "Maria", "Martha", "Job"};
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter name to search: ");
+		String searchName = scanner.nextLine();
+		
+		boolean isFound =false;
+		
+		for(int i=0; i< employees.length; i++) 
+		{
+			if(employees[i].equals(searchName)) {
+				System.out.printf("Employee with name %s has been found", searchName);
+			}else {
+				System.out.println("Employee not found");
+			}
+			
+			scanner.close();
+		}
+	}
+	
 	}
 
-}
